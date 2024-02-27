901997f1d5232374d21fa1a766c93d2f5573f3fd
Oliver <oliver.a.jarjosa@gmail.com>
2024-02-27 17:59:12 -0500

    newest push

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-// const name = 'yoshi';
+const http = require('http');
+const fs = require('fs');
 
-// console.log(name);
 
-const greet = (name) => {
-    console.log('hello, ${name}');
-}
 
-greet('mario');
-greet('yoshi');
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views';
+    switch (req.url) {
+        case = '/': //the root of the website
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case: '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case: '/about-me':
+            
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.statussCode = 200;
+            res.end(data);
+        }
+    })
+
+});
+
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
+
+//3rd party package called Express - will use later on
+//first cover how to install and use 3rd party packages
